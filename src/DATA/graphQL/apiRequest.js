2cfdca73da5e8071823f4fa3ae8932be8f062fc9
Alexandr Ivanitskyi <6saaanchik6@gmail.com>
2022-10-29 12:45:25 +0300

    change api url

--- src/DATA/graphQL/apiRequest.js
+++ src/DATA/graphQL/apiRequest.js
@@ -1,5 +1,5 @@
 export const apiRequest = (query, variables) => {
-    const url = 'https://localhost:7267/graphql';
+    const url = 'https://api-for-myproject.herokuapp.com/graphql';
 
     const response = fetch(url, {
         method: 'POST',
